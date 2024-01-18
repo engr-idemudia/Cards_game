c961ff9bf30203774d8d19a02ad139079dbb8d69
Idemudia M. Osaghae <Idemudiamondayosaghae@gmail.com>
2024-01-18 23:22:16 +0200

    first commit

---
+++ lib/cards.ex
@@ -0,0 +1,61 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # #Solution-1 bad approach
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    # #Solution-2 better and short
+    for suit <- suits, value <- values do  #(Note; start from inside out)
+        "#{value} of #{suit}"
+      end
+  end
+
+  def  shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contain?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+  # #works fine but still too long
+  # def load(filename) do
+  #   {status, binary} = File.read(filename)
+
+  #   case status do
+  #     :ok -> :erlang.binary_to_term (binary)
+  #     :error -> "That file does not exist"
+  #   end
+  # end
+
+  # #most efficient (pattern matching with atom and variable at same time)
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  # #combine the three methods; create_deck(), shuffle(), and deal()
+ # #together by the use of pipe operator
+  def create_hand(hand_size) do
+    Cards.create_deck
+      |> Cards.shuffle
+      |> Cards.deal(hand_size) #note;output of the shuffle is the deck here
+  end
+end
